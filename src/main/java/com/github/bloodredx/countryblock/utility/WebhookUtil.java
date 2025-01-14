5259aba7fff506c248a24803fad15f56de37b3a5
KMDS <aadinathgorg@gmail.com>
2025-01-14 13:18:30 +0530

    Recoded WebhookUtil
    - Added extra features
    - Fixed not working issue

--- src/main/java/com/github/bloodredx/countryblock/utility/WebhookUtil.java
+++ src/main/java/com/github/bloodredx/countryblock/utility/WebhookUtil.java
@@ -1,18 +1,38 @@
 package com.github.bloodredx.countryblock.utility;
 
 import club.minnced.discord.webhook.WebhookClient;
+import club.minnced.discord.webhook.WebhookClientBuilder;
 import club.minnced.discord.webhook.send.WebhookEmbed;
 import club.minnced.discord.webhook.send.WebhookEmbedBuilder;
+import club.minnced.discord.webhook.send.WebhookMessageBuilder;
+
+import java.time.Instant;
 
 public class WebhookUtil {
     private final WebhookClient client;
+    private final String username;
+    private final String avatarUrl;
+    private final String footerText;
+    private final String footerIconUrl;
 
-    public WebhookUtil(String webhookUrl) {
+    public WebhookUtil(String webhookUrl, String username, String avatarUrl, String footerText, String footerIconUrl) {
         if (webhookUrl != null && !webhookUrl.isEmpty()) {
-            this.client = WebhookClient.withUrl(webhookUrl);
+            WebhookClientBuilder builder = new WebhookClientBuilder(webhookUrl);
+            builder.setThreadFactory((job) -> {
+                Thread thread = new Thread(job);
+                thread.setName("CountryBlock Webhook");
+                thread.setDaemon(true);
+                return thread;
+            });
+            this.client = builder.build();
         } else {
             this.client = null;
         }
+        
+        this.username = username;
+        this.avatarUrl = avatarUrl;
+        this.footerText = footerText;
+        this.footerIconUrl = footerIconUrl;
     }
 
     public void sendMessage(String title, String description, int color) {
@@ -22,10 +42,17 @@ public class WebhookUtil {
             .setTitle(new WebhookEmbed.EmbedTitle(title, null))
             .setDescription(description)
             .setColor(color)
+            .setTimestamp(Instant.now())
+            .setFooter(new WebhookEmbed.EmbedFooter(footerText, footerIconUrl))
             .build();
 
+        WebhookMessageBuilder messageBuilder = new WebhookMessageBuilder()
+            .setUsername(username)
+            .setAvatarUrl(avatarUrl)
+            .addEmbeds(embed);
+
         try {
-            client.send(embed).join();
+            client.send(messageBuilder.build()).join();
         } catch (Exception e) {
             e.printStackTrace();
         }
